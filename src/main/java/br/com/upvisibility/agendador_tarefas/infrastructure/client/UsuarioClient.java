5fa01453abd13ae699fb47cfbed3620ef71be9d1
Felipe Oliveira <lipefan0@gmail.com>
2025-09-09 13:42:10 -0300

    corrige apontamentos do client

--- src/main/java/br/com/upvisibility/agendador_tarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/br/com/upvisibility/agendador_tarefas/infrastructure/client/UsuarioClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 )
 public interface UsuarioClient {
 
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDTO buscarUsuarioPorEmail(
             @RequestParam("email") String email,
             @RequestHeader("Authorization") String token
